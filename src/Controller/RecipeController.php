4e7763510475b4b4b13215275f31a87be1eb47c0
Sandrine Orieux <sandrine.orieux@sfr.fr>
2024-01-22 16:17:09 +0100

    add review form and display but not dynamic

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -3,11 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Recipe;
-use App\Entity\User;
-use App\Repository\DietRepository;
-use App\Repository\RecipeRepository;
+use App\Entity\Review;
+use App\Form\ReviewRecipeType;
 use App\Repository\UserRepository;
+use App\Repository\RecipeRepository;
+use App\Repository\ReviewRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -36,11 +39,40 @@ class RecipeController extends AbstractController
     }
 
     #[Route('/recipe/{id}', name: 'app_recipe_show')]
-    public function show($id, RecipeRepository $recipeRepository): Response
+    public function show($id, RecipeRepository $recipeRepository, ReviewRepository $reviewRepository, Request $request, EntityManagerInterface $entityManagerInterface, Security $security): Response
     {
+
+        $user = $security->getUser();
+
         $recipe = $recipeRepository->findOneBy(['id' => $id]);
+
+        $review = $reviewRepository->findOneBy(['recipe' => $recipe, 'user' => $user]);
+
+        if (!$review) {
+            $review = new Review();
+            $review->setRecipe($recipe);
+            $review->setUser($user);
+            $review->setIsValidated(False);
+        }
+
+        $averageRate = ROUND($reviewRepository->getAverageRaterecipeId($recipe->getId()), 2);
+
+        $form = $this->createForm(ReviewRecipeType::class, $review);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManagerInterface->persist($review);
+            $entityManagerInterface->flush();
+        }
+
+
+
+
         return $this->render('recipe/showRecipe.html.twig', [
             'recipe' => $recipe,
+            'form' => $form,
+            'user' => $user,
+            'averageRate' => $averageRate,
         ]);
     }
 }
---
+++ src/Form/ReviewRecipeType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+
+use App\Entity\Review;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReviewRecipeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('rate', ChoiceType::class, [
+                'label' => 'Votre note :',
+                'choices' => [
+                    '5' => 5,
+                    '4' => 4,
+                    '3' => 3,
+                    '2' => 2,
+                    '1' => 1,
+                ],
+                'attr' => [
+                    'class' => 'text-start'
+                ],
+                'expanded' => true,
+                'multiple' => false,
+
+            ])
+            ->add('comment', TextareaType::class, [
+                'label' => 'Donnez votre avis :'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Review::class,
+        ]);
+    }
+}
