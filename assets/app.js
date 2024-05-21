79d0e59b3d65484d557eb27e453f96709f62b867
Sandrine ORIEUX <sandrine.orieux@sfr.fr>
2024-05-21 14:47:00 +0200

    add documentation

--- assets/app.js
+++ assets/app.js
@@ -17,37 +17,25 @@ const reviewForm = document.querySelector('form.js-form-review');
 
 reviewForm.addEventListener('submit', (e) => {
   e.preventDefault();
-
-
-
   const formData = new FormData(e.target);
-
-  const url = window.location.pathname
-
-
+  const url = window.location.pathname;
   axios.post(url, formData)
     .then(function () {
       reviewForm.innerHTML = "<p>Votre évaluation a bien été prise en compte : la note moyenne a été mis à jours, votre commentaire apparaîtra aprés validation d'un administrateur.</p>"
       //take average rate update
       axios.get(urlAverage)
         .then((response) => {
-
           //if success display the average rate in html
           let averageRate = document.querySelector('span.js-average-rate')
           averageRate.innerHTML = response.data['average']
-
         })
         .catch((error) => {
           console.log(error)
         })
     })
     .catch(function (error) {
-
       console.log(error)
     })
-
   const urlAverage = url + '/averageRate';
-
-
 });
 
